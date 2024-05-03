87b2c4d42b4aee790ddaccca66ce6abda4a9d1d8
phuongdoan13 <mphuongdoant@gmail.com>
2024-05-04 00:20:28 +1000

    add (page): add bare skeleton for create-campaign

---
+++ web/app/create-campaign/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Banner from '@/components/layout/banner/banner';
+import Footer from '@/components/layout/footer/Footer';
+import Header from '@/components/layout/header/Header';
+import Main from '@/components/layout/Main';
+
+/**
+ * Use the page component to wrap the components
+ * that you want to render on the page.
+ */
+export default function BuyMeCoffeePage() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  //  Fix hydration issues
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <Header />
+      <Main>
+        <div />
+      </Main>
+      <Footer />
+    </>
+  );
+}

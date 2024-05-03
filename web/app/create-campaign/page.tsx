ad55967b4b2ebd4115e93f0768b7b16d70f70132
phuongdoan13 <mphuongdoant@gmail.com>
2024-05-04 00:23:55 +1000

    update (tidy): removed redundant deps

--- web/app/create-campaign/page.tsx
+++ web/app/create-campaign/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import Banner from '@/components/layout/banner/banner';
 import Footer from '@/components/layout/footer/Footer';
 import Header from '@/components/layout/header/Header';
 import Main from '@/components/layout/Main';

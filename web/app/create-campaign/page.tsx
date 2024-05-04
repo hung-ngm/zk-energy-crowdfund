4888600967d8c81cf6805498099337ebbe539338
phuongdoan13 <mphuongdoant@gmail.com>
2024-05-04 22:45:20 +1000

    update: change name and removed redundant code

--- web/app/create-campaign/page.tsx
+++ web/app/create-campaign/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import Banner from '@/components/layout/banner/banner';
 import Footer from '@/components/layout/footer/Footer';
 import Header from '@/components/layout/header/Header';
 import Main from '@/components/layout/Main';
@@ -11,7 +10,7 @@ import BuyMeCoffeeContractDemo from './_components/ContractDemo';
  * Use the page component to wrap the components
  * that you want to render on the page.
  */
-export default function BuyMeCoffeePage() {
+export default function CreateCampaign() {
   const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
@@ -27,7 +26,6 @@ export default function BuyMeCoffeePage() {
     <>
       <Header />
       <Main>
-      <Banner pageName="Create Campaign" pageUrl="create-campaign" />
         <BuyMeCoffeeContractDemo />
       </Main>
       <Footer />

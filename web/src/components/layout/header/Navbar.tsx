09990b576fb7b1893a4cbc4034406db06803b195
hung-ngm <prohungnguyenminh@gmail.com>
2024-05-04 10:06:59 +1000

    feat: added campaigns in the navbar

--- web/src/components/layout/header/Navbar.tsx
+++ web/src/components/layout/header/Navbar.tsx
@@ -71,6 +71,9 @@ function Navbar() {
               <NavbarLink href="/#get-started">Get Started</NavbarLink>
             </li>
             <li className="flex">
+              <NavbarLink href="/campaigns">Campaigns</NavbarLink>
+            </li>
+            <li className="flex">
               <NavbarLink href="/create-campaign">Create Campaign</NavbarLink>
             </li>
             <li className="flex">

7bd85514a68843c83ccad29ffe5ba0a5e711ccc4
hung-ngm <prohungnguyenminh@gmail.com>
2024-05-05 00:30:02 +1000

    WIP bug erc20 approve

--- contracts/export_abi.sh
+++ contracts/export_abi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set the path to your JSON file
-JSON_FILE="out/ZKGreenEnergyCrowdFunding.sol/ZKGreenEnergyCrowdFunding.json"
+JSON_FILE="out/ERC20Token.sol/ERC20Token.json"
 
 # Set the output file path for the ABI
-OUTPUT_FILE="ZKGreenEnergyCrowdFunding.abi"
+OUTPUT_FILE="ERC20Token.abi"
 
 # Extract the ABI from the JSON file and save it to the output file
 jq -r '.abi' "$JSON_FILE" > "$OUTPUT_FILE"

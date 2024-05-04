03e6b289333eec89e88cea50f97dcbf715cf5583
Harry Doan <35623720+phuongdoan13@users.noreply.github.com>
2024-05-04 14:44:05 +1000

    Revert "Add nextauth to support worldid"

--- web/app/create-campaign/page.tsx
+++ web/app/create-campaign/page.tsx
@@ -1,21 +1,31 @@
-import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth/next";
+'use client';
+
+import { useEffect, useState } from 'react';
 import Footer from '@/components/layout/footer/Footer';
 import Header from '@/components/layout/header/Header';
 import Main from '@/components/layout/Main';
-export default async function CreateCampaign() {
-  const session = await getServerSession();
-  if (!session?.user) {
-    redirect("/api/auth/signin");
+
+/**
+ * Use the page component to wrap the components
+ * that you want to render on the page.
+ */
+export default function BuyMeCoffeePage() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  //  Fix hydration issues
+  if (!isMounted) {
+    return null;
   }
 
   return (
     <>
       <Header />
       <Main>
-        This is a protected route.
-        <br />
-        You will only see this if you are authenticated.
+        <div />
       </Main>
       <Footer />
     </>
